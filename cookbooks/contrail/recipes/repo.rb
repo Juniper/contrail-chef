df078b28b05136703facdd24c2ff65506a54bf86
Adrian Smith <adrian.smith@workday.com>
2015-03-19 11:26:36 +0000

    Use yum cookbook to define contrail repo
    By using the yum cookbook we can provide the URL of the contrail repo as
    an attrbute.

--- cookbooks/contrail/recipes/repo.rb
+++ cookbooks/contrail/recipes/repo.rb
@@ -5,9 +5,19 @@
 # Copyright 2014, Juniper Networks
 #
 
-if not platform?("ubuntu") then
-    template "/etc/yum.repos.d/contrail-install.repo" do
-        source "contrail-centos.repo.erb"
-        mode 00644
-    end
+# We're using yum-priorities to ensure packages in the contrail repo are used
+# over and above newer versions that may exist in other repos.
+package "yum-plugin-priorities" do
+  action :install
+end
+
+case node["platform_family"]
+when "rhel"
+  yum_repository "contrail" do
+    description "Contrail Packages"
+    baseurl node["contrail"]["yum_repo_url"]
+    gpgcheck false
+    priority "1"
+    action :create
+  end
 end
